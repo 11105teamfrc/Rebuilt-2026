a46a2d1cca4cb9ddc18d4cd9577f138879073fc7
Noah Ramos <barrosrnoah@gmail.com>
2026-02-09 09:48:06 -0300

    auto2 jully 09/02

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.Auto;
+
 public final class Constants {
 
   public static final class DriveConstants {
--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -4,43 +4,38 @@
 //
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.CANDriveSubsystem;
 import frc.robot.subsystems.CANFuelSubsystem;
 
 public class Auto extends SequentialCommandGroup {
-    
     public Auto(CANDriveSubsystem drive, CANFuelSubsystem fuel) {
-
         addCommands(
-        
-            new RunCommand(
-                () -> drive.driveArcade(
-                    () -> 4.0,
-                    () -> 0.0
-                ),
-                drive
-            ).withTimeout(2.0), 
             
-            new InstantCommand(
-                () -> drive.driveArcade(
-                    () -> 0.0,
-                    () -> 0.0
-                ),
+            new RunCommand(() -> drive.driveArcade(
+                () -> 0.3, 
+                () -> 0.0), 
                 drive
-            ),
-
+                ).withTimeout(2.0),
+            
+            new InstantCommand(() -> drive.driveArcade(
+                () -> 0.0, 
+                () -> 0.0), 
+                drive), 
+            
             new WaitCommand(0.3),
-
-            new InstantCommand(
-                fuel::launch,
+                
+            new RunCommand(
+                fuel::launch, 
                 fuel
+            ).withTimeout(1.0), 
+            
+            new InstantCommand(fuel::stop, fuel
             )
+        
         );
     }
 }
